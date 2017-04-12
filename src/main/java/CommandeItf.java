18088215e155f081d2971fb1ae5bc1217d3b4bcd
tiret <tiretfabien@hotmail.fr>
2017-04-12 08:58:20 +0200

    mis en commun interface de commande

---
+++ src/main/java/CommandeItf.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import java.util.ArrayList;
+
+public interface CommandeItf<T> {
+
+	public String LPUSH(String key, ArrayList<T> value); 
+	public String RPUSH(String  key, ArrayList<T> value); 
+	public String LPUSHX(String key, ArrayList<T> value); 
+	public String RPUSHX(String key, ArrayList<T> value); 
+
+	public String LINSERT(String key, String mode, T value, T insert); 
+	public String LPOP(String key); 
+	public String RPOP(String key); 
+	public String LLEN(String key); 
+	public String LSET(String key, int index, T value); 
+	public String SREM(String key, ArrayList<String> value); 
+	
+	public String APPEND(String key, T value); 
+	public String DECR(String key); 
+	public String INCR(String key); 
+
+}
---
+++ src/main/java/Commandes.java
@@ -0,0 +1,15 @@
+package main.java;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Commandes {
+	HashMap<String, ArrayList<String>> stock;
+	
+	public Commandes(){
+		stock = new HashMap<String,ArrayList<String>>();
+	}
+	
+	
+}
