a09472cfd13267e318ce65bff2c933df78d7b5ac
gaurang1589 <Gaurang@wificampus-030220.grenet.fr>
2017-04-12 10:50:49 +0200

    Added Class Command Test
    Create Command test class

---
+++ src/main/test/CommandTest.java
@@ -0,0 +1,82 @@
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandTest {
+
+	@Before
+	@Test
+	public void setUp() throws Exception {
+	}
+
+	@After
+	@Test
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void LPUSH(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void RPUSH(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void LPUSHX(){
+		fail("Not yet implemented");
+	}
+	@Test
+	public void RPUSHX(){
+		fail("Not yet implemented");
+	} 
+
+	@Test
+	public void LINSERT(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void LPOP(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void RPOP(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void LLEN(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void LSET(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void SREM(){
+		fail("Not yet implemented");
+	} 
+	
+	@Test
+	public void APPEND(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void DECR(){
+		fail("Not yet implemented");
+	} 
+	@Test
+	public void INCR(){
+		fail("Not yet implemented");
+	}
+
+}
