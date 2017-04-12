8b35c1a33117bd921a9a8df08db46ff64cdccb06
tiret <tiretfabien@hotmail.fr>
2017-04-12 10:23:12 +0200

    implementation de quelque commandes

--- src/main/java/CommandeItf.java
+++ src/main/java/CommandeItf.java
@@ -1,23 +1,24 @@
 package main.java;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public interface CommandeItf<T> {
 
-	public String LPUSH(String key, ArrayList<T> value); 
-	public String RPUSH(String  key, ArrayList<T> value); 
-	public String LPUSHX(String key, ArrayList<T> value); 
-	public String RPUSHX(String key, ArrayList<T> value); 
+	public String LPUSH(String key, LinkedList<T> value); //push left 
+	public String RPUSH(String  key, LinkedList<T> value);//push right 
+	public String LPUSHX(String key, LinkedList<T> value); //push left if exist
+	public String RPUSHX(String key, LinkedList<T> value); //push right if exist
 
 	public String LINSERT(String key, String mode, T value, T insert); 
 	public String LPOP(String key); 
 	public String RPOP(String key); 
 	public String LLEN(String key); 
 	public String LSET(String key, int index, T value); 
-	public String SREM(String key, ArrayList<String> value); 
+	public String SREM(String key, LinkedList<String> value); 
 	
 	public String APPEND(String key, T value); 
 	public String DECR(String key); 
-	public String INCR(String key); 
+	public String INCR(String key);
+
 
 }
--- src/main/java/Commandes.java
+++ src/main/java/Commandes.java
@@ -1,15 +1,121 @@
 package main.java;
 
-
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.HashMap;
 
-public class Commandes {
-	HashMap<String, ArrayList<String>> stock;
+public class Commandes implements CommandeItf<String>{
+	HashMap<String, LinkedList<String>> stock;
 	
 	public Commandes(){
-		stock = new HashMap<String,ArrayList<String>>();
+		stock = new HashMap<String,LinkedList<String>>();
 	}
-	
-	
+
+	@Override
+	public String LPUSH(String key, LinkedList<String> value) {
+		if(stock.containsKey(key)){	
+			LinkedList<String> list = stock.get(key);
+			value.addAll(list);
+			stock.put(key, value);
+			
+			return String.valueOf(value.size());
+		}else{
+			stock.put(key, value);
+			return String.valueOf(value.size());
+		}
+	}
+	@Override
+	public String RPUSH(String key, LinkedList<String> value) {
+		if(stock.containsKey(key)){	
+			LinkedList<String> list = stock.get(key);
+			list.addAll(value);
+			stock.put(key, list);
+			
+			return String.valueOf(list.size());
+		}else{
+			stock.put(key, value);
+			return String.valueOf(value.size());
+		}
+		
+	}
+
+	@Override
+	public String LPUSHX(String key, LinkedList<String> value) {
+		if(stock.containsKey(key)){	
+			LinkedList<String> list = stock.get(key);
+			value.addAll(list);
+			stock.put(key, value);
+			
+			return String.valueOf(value.size());
+		}else{
+			return "0";
+		}
+	}
+
+	@Override
+	public String RPUSHX(String key, LinkedList<String> value) {
+		if(stock.containsKey(key)){	
+			LinkedList<String> list = stock.get(key);
+			list.addAll(value);
+			stock.put(key, list);
+			
+			return String.valueOf(list.size());
+		}else{
+			return "0";
+		}
+	}
+
+	@Override
+	public String LINSERT(String key, String mode, String value, String insert) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String LPOP(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String RPOP(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String LLEN(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String LSET(String key, int index, String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String SREM(String key, LinkedList<String> value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String APPEND(String key, String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String DECR(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String INCR(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
