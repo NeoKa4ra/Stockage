5829172dd5976077580971d83d3e725858e31685
neoka4ra <navarro.romain.j@gmail.com>
2017-04-12 11:04:30 +0200

    Ajout de Transaction + avancement de Menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -2,5 +2,31 @@ package main.java;
 
 public class Menu{
 	
+	static final int MENU 0;
+	static final int COMMANDES 1;
+	static final int MANIPULATE 2;
 	
+	
+	Menu(){
+		
+	}
+	
+	Public String dialog(String state){
+		String sentence;
+		switch(state){
+			case MENU :
+				sentence = "Menu :" + "1 : Commandes" + "2 : Manipulation";
+				break;
+			case COMMANDES :
+				sentence = "";
+				break;
+			case MANIPULATE :
+				sentence = "Entrée incorrecte";
+				break;
+			default :
+				sentence = "Entrée incorrecte";
+
+		}
+		return sentence;
+	}
 }
